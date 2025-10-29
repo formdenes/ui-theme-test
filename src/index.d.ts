9f2aa24a3d1b042a810f4ea4b9c1d3657a6fa718
Formanek Dénes <formdenes@gmail.com>
2025-10-29 17:11:26 +0100

    Added toggle button size override module export

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,9 @@
 import { Theme } from "@mui/material";
 
 export declare const hclTheme: Theme;
+
+export declare module "@mui/material/ToggleButtonGroup" {
+  interface ToggleButtonGroupPropsSizeOverrides {
+    mini: true;
+  }
+}
